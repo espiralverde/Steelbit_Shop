84a973a0087d15630d1cfbd691664b2207c297f4
espiralverde <martinsosa@outlook.com>
2023-08-31 18:59:06 -0300

    fix filtro ascendente

--- frontend/src/components/SortOptionsComponent.js
+++ frontend/src/components/SortOptionsComponent.js
@@ -8,8 +8,8 @@ const SortOptionsComponent = ({setSortOption}) => {
         >
         
             <option>Ordernar</option>
-            <option value="precio_1">Ascendente</option>
-            <option value="precio_-1">Descendente</option>
+            <option value="price_1">Ascendente</option>
+            <option value="price_-1">Descendente</option>
             <option value="rating_-1">Calificación</option>
             <option value="name_1">A - Z</option>
             <option value="name_-1">Z - A</option>
